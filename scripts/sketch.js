9f56664b2b70ce4d34ab7812db88388226805253
alfathzikir <aifzikir@gmail.com>
2021-01-29 00:48:13 -0700

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,55 +1,13 @@
-/*
- * @name Animation
- * @description The circle moves.
- */
-// Where is the circle
-let x, y;
-let watermelon_1;
-let watermelon_2;
-let guy1, guy2;
-
+let img; // Declare variable 'img'.
 
 function setup() {
   createCanvas(720, 400);
-  // Starts in the middle
-  x = width / 2;
-  y = height;
-
-  watermelon_1 = loadImage("../assets/watermelon.png");
-
-  myButton = new Clickable();     //Create button
-  myButton.locate(x/2, 20);        //Position Button
-  myButton.text = "EXPLOSION!";
-  myButton.onPress = function(){  //When myButton is pressed
-    this.color = "#AAAAFF";       //Change button color
-    alert("testttttttttttttttt!");                //Show an alert message
-  }
-  
-
-
-
-
+  img = loadImage('../assets/watermelon.jpg'); // Load the image
 }
 
 function draw() {
-
-  image(watermelon_1, 0, height / 2, watermelon_1.width / 2, watermelon_1.height / 2);
-  
-  // Draw a circle
-  stroke(50);
-  fill(100);
-  ellipse(x, y, 24, 24);
-  
-  // Jiggling randomly on the horizontal axis
-  x = x + random(-1, 1);
-  // Moving up at a constant speed
-  y = y - 1;
-  
-  // Reset to the bottom
-  if (y < 0) {
-    y = height;
-  }
-
-  myButton.draw();
+  // Displays the image at its actual size at point (0,0)
+  image(img, 0, 0);
+  // Displays the image at point (0, height/2) at half size
+  image(img, 0, height / 2, img.width / 2, img.height / 2);
 }
-
